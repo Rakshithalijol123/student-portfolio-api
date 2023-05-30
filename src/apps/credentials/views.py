6ec00feab219553a42dc0e6cbd79eb8ec5e6f915
Rakshit Halijol <20beam031@dsce.edu.in>
2023-05-30 17:10:33 +0530

    Changes done in views file

--- src/apps/credentials/views.py
+++ src/apps/credentials/views.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
-from .urls import USER_CREATE_PATH
+from .urls import USER_CREATE_PATH, GET_USER, LOGIN_USER, LOGOUT_USER
 import src.database.interaction as db
+
+import bcrypt
+
 from src.models.requests import (
-    UserDataModel
+    UserDataModel, LoginModel
 )
 from src.models.responses import (
     UserDataModelResponse
@@ -10,7 +13,35 @@ from src.models.responses import (
 router = APIRouter()
 
 
-@router.post(USER_CREATE_PATH)
+@router.post(
+    USER_CREATE_PATH,
+    description="This end point helps to store user data in database"
+
+)
 def create_new_user(userData: UserDataModel) -> UserDataModelResponse:
     inserted_id = db.create_user(dict(userData))
     return {"status": "OK", "inserted_id": inserted_id}
+
+
+@router.get(GET_USER)
+def get_user(username: str, password: str):
+    user_password = db.get_user(username)
+    try:
+        check = bcrypt.checkpw(password.encode(
+            'utf-8'), user_password.encode('utf-8'))
+    except:
+        check = password == user_password
+
+    return {"check": check}
+
+
+@router.patch(LOGIN_USER)
+def login_user(loginData: LoginModel):
+    status = db.login(loginData.username, loginData.password)
+    return {"state": status}
+
+
+@router.patch(LOGOUT_USER)
+def logout_user(email: str):
+    status = db.logout(email)
+    return {"state": status}
