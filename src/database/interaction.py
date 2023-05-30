7c1b59a386955bcc44f0648ca5edcd70a704f2e4
Rakshit Halijol <20beam031@dsce.edu.in>
2023-05-30 17:11:02 +0530

    Changes done in db interaction

--- src/database/interaction.py
+++ src/database/interaction.py
@@ -2,9 +2,10 @@ from .connection import (
     connect,
     create_database
 )
+import bcrypt
 
 client = connect("mongodb://localhost:27017")
-db = create_database(client=client, db_name="TEST-DATABASE")
+db = create_database(client=client, db_name="STUDENT-PORTFOLIO-DATABASE")
 
 
 # --- Create your collections here ---
@@ -16,3 +17,43 @@ User = db["User"]
 def create_user(userData: dict) -> str:
     user = User.insert_one(userData)
     return str(user.inserted_id)
+
+
+def get_user(username: str) -> str:
+    user = User.find_one({"username": username})
+    return str(user["password"])
+
+
+def login(username: str, password: str) -> bool:
+    try:
+        hashed_password = get_user(username)
+        print(hashed_password)
+    except:
+        return False
+    password_status = bcrypt.checkpw(password.encode(
+        'utf-8'), hashed_password.encode('utf-8'))
+    print(password_status)
+    if password_status == False:
+        return False
+    filter: dict(str) = {"username": username, "password": hashed_password}
+    update_user: dict(str) = {"isActive": True}
+    try:
+        login_user = User.update_one(filter, {"$set": update_user})
+        print(f"acknowledged = {login_user.acknowledged} ")
+        if login_user.acknowledged:
+            return True
+        return False
+    except:
+        return None
+
+
+def logout(email: str) -> bool:
+    filter: dict(str) = {"email": email}
+    update_user: dict(str) = {"isActive": False}
+    try:
+        logout_user = User.update_one(filter, {"$set": update_user})
+        if logout_user.acknowledged:
+            return True
+        return False
+    except:
+        return False
